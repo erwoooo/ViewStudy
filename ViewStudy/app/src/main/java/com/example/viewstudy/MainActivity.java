6b4131c969e7e56083e675ab502a62a7f9b81652
wei.chen <wei.chen@xdynamics.com.cn>
2020-06-10 17:30:47 +0800

    second

---
+++ ViewStudy/app/src/main/java/com/example/viewstudy/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.viewstudy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
